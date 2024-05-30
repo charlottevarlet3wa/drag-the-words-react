90a02564c73fb3667e37aa32cade566fc9fc14f5
Charlotte Varlet <c.varlet@3wa.fr>
2024-05-30 12:03:49 +0200

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Game from './component/Game';
 
 function App() {
 
   return (<>
-      <BrowserRouter>
-        {/* <Game /> */}
+      <Router>
+      <Switch>
+        <Route path="/1" component={<Game/>} />
+      </Switch>
+    </Router>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/:textId" element={<Game />} />
           <Route path="*" element={<Game />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
   </>
   );
 }
